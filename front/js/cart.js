dd783e46930eef29a64699291c32a0508e84eb50
OrelOreo <oreloreopro@gmail.com>
2023-03-20 17:49:39 +0100

    correction final

---
+++ front/js/cart.js
@@ -0,0 +1,243 @@
+const sectionCartItems = document.querySelector('#cart__items')
+let totalPriceElement = document.querySelector('#totalPrice')
+let totalQuantity = document.querySelector('#totalQuantity')
+let totalArticlesPrice = 0
+let totalArticlesQuantity = 0
+
+async function getProductById(artId) {
+    try {
+        const res = await fetch(`http://localhost:3000/api/products/${artId}`)
+        const response = await res.json()
+        if(response._id === artId) {
+            return response
+        }
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+const products = JSON.parse(localStorage.getItem('products'))
+
+// Pour chaque produit dans le localStorage, on affiche les informations de chaque produit
+let priceElement
+async function showCart() {
+    for (let i = 0; i < products.length; i++) {
+        priceElement = await getProductById(products[i].id)
+        let {name, imageUrl, altTxt} = await getProductById(products[i].id)
+        totalArticlesQuantity += parseInt(products[i].inputQuantity)
+        totalArticlesPrice += parseInt(products[i].inputQuantity * priceElement.price)
+
+        let article = `
+                        <article class="cart__item" data-id="${products[i].id}" data-color="${products[i].selectColor}">
+                            <div class="cart__item__img">
+                                <img src="${imageUrl}" alt="${altTxt}">
+                            </div>
+                            <div class="cart__item__content">
+                                <div class="cart__item__content__description">
+                                    <h2>${name}</h2>
+                                    <p>${products[i].selectColor}</p>
+                                    <p>${priceElement.price} €</p>
+                                </div>
+                                <div class="cart__item__content__settings">
+                                    <div class="cart__item__content__settings__quantity">
+                                        <p>Qté: </p>
+                                        <input data-id="${products[i].id}" data-color="${products[i].selectColor}" type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${products[i].inputQuantity}">
+                                    </div>
+                                    <div class="cart__item__content__settings__delete">
+                                        <p data-id="${products[i].id}" data-color="${products[i].selectColor}" class="deleteItem">Supprimer</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+                      `
+        sectionCartItems.innerHTML += article
+        totalQuantity.innerText = totalArticlesQuantity
+        totalPriceElement.innerText = totalArticlesPrice
+    }
+
+
+// Ajouter un écouteur d'événement input sur tous les éléments .itemQuantity
+    const itemQuantityInputs = document.querySelectorAll('.itemQuantity')
+    itemQuantityInputs.forEach(input => {
+    input.addEventListener('input', (event) => {
+        const productId = event.target.dataset.id
+        const productColor = event.target.dataset.color
+        const newQuantity = parseInt(event.target.value)
+
+        // Mettre à jour la quantité et le prix total du produit dans le panier
+        const products = JSON.parse(localStorage.getItem('products'))
+        const product = products.find(p => p.id === productId && p.selectColor === productColor)
+        const oldQuantity = product.inputQuantity
+        const productPrice = priceElement.price
+        product.inputQuantity = newQuantity
+        const quantityDifference = newQuantity - oldQuantity
+        totalArticlesQuantity += quantityDifference
+        totalArticlesPrice += quantityDifference * productPrice
+        // Enregistrer les modifications dans localStorage
+        localStorage.setItem('products', JSON.stringify(products))
+
+        // Mettre à jour le texte des éléments totalQuantity et totalPriceElement
+        totalQuantity.innerText = totalArticlesQuantity
+        totalPriceElement.innerText = totalArticlesPrice
+    })
+ })
+     // Suppression des articles dans le DOM et également du localStorage
+     const deleteButtons = document.querySelectorAll('.deleteItem')
+     deleteButtons.forEach(deleteButton => {
+     deleteButton.addEventListener('click', (event) => {
+         const articleToDelete = event.target.closest('.cart__item')
+         const productId = event.target.dataset.id
+         const productColor = event.target.dataset.color
+         articleToDelete.remove()
+ 
+         const products = JSON.parse(localStorage.getItem('products'))
+
+         const product = products.find(p => p.id === productId && p.selectColor === productColor)
+         const productQuantity = product.inputQuantity
+         const productPrice = priceElement.price
+         const totalPriceToRemove = productPrice * productQuantity
+
+         // Trouver l'index de l'article dans la liste de produits dans le localStorage
+         const articleIndex = products.findIndex(product => product.id === productId && product.selectColor === productColor)
+         
+         // Supprimer de cette liste avec .splice, 1er arguement l'index du produit, 2nd argument combien d'élément je supprime
+         products.splice(articleIndex, 1)
+         // Mettre a jour le localStorage avec la nouvelle liste
+         localStorage.setItem('products', JSON.stringify(products))
+
+         totalQuantity.innerText -= productQuantity
+         totalPriceElement.innerText -= totalPriceToRemove
+     })
+ })
+
+}
+
+
+showCart()
+
+
+// FORM REGEX
+
+const validationForm = {
+    firstName: {
+      inputElementForm: document.getElementById("firstName"),
+      errorElement : document.getElementById('firstNameErrorMsg'),
+      // cette expression régulière permet de vérifier que la chaîne de caractères ne contient que des lettres, sans espaces ni autres caractères, et que la chaîne commence et se termine par une lettre.
+      regex: /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/,
+      errorMsg: "Prénom invalide",
+    },
+    lastName: {
+      inputElementForm: document.getElementById("lastName"),
+      errorElement : document.getElementById('lastNameErrorMsg'),
+      // cette expression régulière permet de vérifier que la chaîne de caractères ne contient que des lettres, sans espaces ni autres caractères, et que la chaîne commence et se termine par une lettre.
+      regex: /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/,
+      errorMsg: "Nom invalide",
+    },
+    address: {
+      inputElementForm: document.getElementById("address"),
+      errorElement : document.getElementById('addressErrorMsg'),
+      // cette expression régulière permet de vérifier que la chaîne de caractères commence par un nombre entier compris entre 1 et 99999, suivi d'une séquence de caractères qui peut inclure des lettres, des chiffres, des espaces, des virgules, des apostrophes et des tirets. La chaîne peut également inclure jusqu'à trois séquences supplémentaires de ce type, mais ces séquences sont optionnelles.
+      regex: /^\d{1,5} [A-Za-z0-9\s,'-]{1,30}(?: [A-Za-z\s,'-]+){0,3}$/,
+      errorMsg: "Adresse invalide",
+    },
+    city: {
+      inputElementForm: document.getElementById("city"),
+      errorElement : document.getElementById('cityErrorMsg'),
+      // cette expression régulière permet de vérifier que la chaîne de caractères ne contient que des lettres, sans espaces ni autres caractères, et que la chaîne commence et se termine par une lettre.
+      regex: /^[a-zA-ZÀ-ÖØ-öø-ÿ]/,
+      errorMsg: "Ville invalide",
+    },
+    email: {
+      inputElementForm: document.getElementById("email"),
+      errorElement : document.getElementById('emailErrorMsg'),
+      // cette expression régulière permet de vérifier que la chaîne de caractères est une adresse e-mail valide, qui commence par un nom d'utilisateur qui peut inclure des lettres, des chiffres, des points, des soulignés, des plus et des moins, suivi du caractère @ et du nom de domaine qui peut inclure des lettres, des chiffres et des points.
+      regex: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]/,
+      errorMsg: "Email invalide",
+    },
+  };
+
+  // Submit Form
+
+  const inputFirstName = document.querySelector('#firstName')
+  inputFirstName.addEventListener('change', () => checkValidInput(validationForm.firstName))
+
+  const inputLastName = document.querySelector('#lastName')
+  inputLastName.addEventListener('change', () => checkValidInput(validationForm.lastName))
+
+  const inputAdress = document.querySelector('#address')
+  inputAdress.addEventListener('change', () => checkValidInput(validationForm.address))
+
+  const inputCity = document.querySelector('#city')
+  inputCity.addEventListener('change', () =>  checkValidInput(validationForm.city))
+
+  const inputEmail = document.querySelector('#email')
+  inputEmail.addEventListener('change', () => checkValidInput(validationForm.email))
+
+
+  // Vérification des valeurs des inputs
+  // Si les valeurs des inputs ne correspondent pas au regex alors retourne un message d'erreur
+  function checkValidInput(input) {
+    const element = input.inputElementForm
+    const regex = input.regex
+    const errorElement = input.errorElement
+    const errorMsg = input.errorMsg
+    const isValid = regex.test(element.value)
+    if (!isValid) {
+        errorElement.innerText = errorMsg
+    } else {
+        errorElement.innerText = ""
+    }
+    return isValid
+  }
+
+  // Formulaire de confirmation qui enverra les données au serveur avec la route "POST"
+
+  const sendForm = async (contact, products) => {
+    const response = await fetch ("http://localhost:3000/api/products/order", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(contact, products)
+    })
+    const dataResponse = await response.json()
+    return dataResponse
+    
+  }
+
+  document.querySelector('.cart__order__form').addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    let contact = {
+        firstName: inputFirstName.value,
+        lastName: inputLastName.value,
+        address: inputAdress.value,
+        city: inputCity.value,
+        email: inputEmail.value
+    }
+    checkValidInput(validationForm.firstName)
+    checkValidInput(validationForm.lastName)
+    checkValidInput(validationForm.address)
+    checkValidInput(validationForm.city)
+    checkValidInput(validationForm.email)
+
+    // si le panier est vide alors une alerte prévient l'utilisateur que c'est impossible de passer la commande
+    if (products === null || products.length == 0) {
+        alert('Votre panier est vide, impossible de passer la commande')
+    } else if (
+        checkValidInput(validationForm.firstName) === false ||
+        checkValidInput(validationForm.lastName) === false ||
+        checkValidInput(validationForm.address) === false ||
+        checkValidInput(validationForm.city) === false ||
+        checkValidInput(validationForm.email) === false
+    ) {
+        return
+    }
+    sendForm({ contact, products: products.map((product) => product.id)})
+        .then((data) => {
+            localStorage.removeItem('products')
+            window.location.href = "confirmation.html?id=" + data.orderId
+        })
+   
+
+  })
---
+++ front/js/product.js
@@ -0,0 +1,103 @@
+// La propriété search de l'interface "location" est un query string
+// C'est une string contenant "?" suivi par les params de l'URL
+const paramsString = window.location.search
+
+// L'intérface URLSearchParams définit des méthodes utilitaires pour travailler avec la chaine de requete (get)
+const urlParams = new URLSearchParams(paramsString)
+
+// La méthode get() de URLSearchParams retourne la première valeur associé au param donné
+const idProduct = urlParams.get('id')
+
+fetch(`http://localhost:3000/api/products/${idProduct}`)
+    .then(response => response.json())
+    .then((data) => genererProduct(data))
+
+// Génération du produit avec ses informations
+function genererProduct(product) {
+    const colors = product
+    const description = document.querySelector('#description')
+    description.innerText = product.description
+    const {name, price} = product
+    const {imageUrl, altTxt} = product
+    genererImageProduct(imageUrl, altTxt)
+    genererNameAndPriceProduct(name, price)
+    genererColorsProduct(colors)
+}
+// Génération de l'image et de son text alternatif
+function genererImageProduct(imageUrl, altTxt) {
+    const imageElement = document.querySelector('.item__img')
+    const imageProduct = document.createElement('img')
+    imageProduct.src = imageUrl
+    imageProduct.alt = altTxt
+    imageElement.appendChild(imageProduct)
+}
+// Génération du nom et du prix du produit
+function genererNameAndPriceProduct(name, price) {
+    const titleElement = document.querySelector('#title')
+    titleElement.innerHTML = name
+    const priceElement = document.querySelector('#price')
+    priceElement.innerHTML = price
+}
+// Génération des différentes couleurs dispo du produit
+function genererColorsProduct(colors) {
+    const selectElements = document.querySelector('#colors')
+    // Pour chaque élém du tableau colors creer balise option en attribuant les élém venant du tableau
+    // colors.colors parce que le 1er corréspond au produit et le 2nd correspond au nom du tableau
+    colors.colors.forEach(color => {
+        const option = document.createElement('option')
+        option.value = color
+        option.textContent = color
+        selectElements.appendChild(option)
+    })
+}
+
+// Add-To-Cart Event
+
+const addToCartButton = document.querySelector('#addToCart')
+
+// Bouton qui permet de récupérer les valeurs des couleurs et de la quantité et également d'ajouter au panier(localStorage)
+addToCartButton.addEventListener('click',  function (event) {
+    const selectColor = document.querySelector('#colors').value
+    const inputQuantity = document.querySelector('#quantity').value
+    saveStorage(selectColor,inputQuantity)
+})
+
+function saveStorage(selectColor, inputQuantity) {
+    var storage = {
+        id: idProduct,
+        selectColor: selectColor,
+        inputQuantity: Number (inputQuantity),
+    }
+    if (inputQuantity > 100 || inputQuantity < 1){
+        alert('Veuillez choisir une quantité entre 1 et 100')
+    } else if (selectColor === "") {
+        alert("Veuillez choisir une couleur")
+    }
+    else {
+        conditionnalStorage(selectColor, inputQuantity, storage)
+    }
+}
+
+function conditionnalStorage(selectColor, inputQuantity, storage) {
+    let products = localStorage.getItem('products') || '[]'
+    products = JSON.parse(products)
+    
+    let productIndex = products.findIndex((p) => p.id === storage.id && p.selectColor === storage.selectColor)
+  
+    if (productIndex !== -1) {
+       let newQuantity =  products[productIndex].inputQuantity + storage.inputQuantity
+       if (newQuantity > 100 || newQuantity < 1) {
+        alert('Veuillez choisir une quantité entre 1 et 100')
+       } else {
+            newQuantity + products[productIndex].inputQuantity
+            products[productIndex].inputQuantity = newQuantity 
+       }
+    } else if (selectColor === "" || inputQuantity < 1) {
+        alert("Veuillez choisir une couleur et séléctionner le nombre d'articles")
+    } else {
+        products.push(storage)
+        alert("Votre produit a bien été ajouté dans le panier")
+    }
+    localStorage.setItem('products', JSON.stringify(products))
+
+}
